5adf8c99bd9cba24b057703e0e67181dcda245ee
Naoko-I <naoko.i1024@gmail.com>
2020-02-26 09:21:24 +0000

    add admin/genre/index

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,19 +1,29 @@
 class Admin::GenresController < ApplicationController
 	def index
+		@genre = Genre.new
+		@genres = Genre.all
+		genre = Genre.find(params[:id])
 	end
 
 	def create
+		@genre = Genre.new(genre_params)
 	end
 
 	def edit
-		@genres = Genre.find(params[:id])
+		@genre = Genre.find(params[:id])
 	end
 
 	def update
+		@genre = Genre.find(params[:id])
+    	if @genre.update(genre_params)
+      	   redirect_to admin_genres_path
+    	else
+           render :edit
+        end
 	end
 
 	private
-	def genres_params
+	def genre_params
 		params.require(:genres).permit(:name, :is_displayed)
 	end
 end
