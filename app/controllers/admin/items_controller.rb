c0a222786e1998d1c79f91eae925655241bd8eba
s-ito-coder <holyarrow109@gmail.com>
2020-02-27 12:10:24 +0900

    [Add]items_index

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,8 +1,14 @@
 class Admin::ItemsController < ApplicationController
 	def new
+		@item = Item.new
 	end
 
 	def create
+		# 新規登録するためのItemオブジェクトの作成と取得値代入
+		item = Item.new( item_params )
+		# 投稿内容の保存
+		item.save
+		redirect_to admin_item_path( item )
 	end
 
 	def index
@@ -15,5 +21,11 @@ class Admin::ItemsController < ApplicationController
 	end
 
 	def update
+	end
+
+	private
+	def item_params
+		# :imge_idではなく:imageっぽい
+		params.require(:item).permit( :genre_id, :name, :introduction, :listed_price, :image, :selling_status )
 	end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
 	def index
+		items = Item.where(selling_status: true)
+		@pages = items.all.page(params[:page]).reverse_order
+		@counts = items.count
 	end
 
 	def show
