aaaffdd0f6c16f3cbf75a51b509ecd4e76562e95
藤野貴明 <fujinotakaaki@fujinotakaakinoMacBook-Pro.local>
2020-03-03 17:24:29 +0900

    add varidation to user postal_code

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -6,8 +6,9 @@ class EndUser < ApplicationRecord
   has_many :addresses
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :authentication_keys => [ :email, :is_deleted ]
-  columns = %w? first_name last_name first_name_kana last_name_kana postal_code address phone_number email ?
+  columns = %w? first_name last_name first_name_kana last_name_kana address phone_number email ?
   columns.each do |column|
     eval( %?validates :#{column}, presence: true? )
   end
+  validates :postal_code, presence: true, format: { with: /\A\d{7}\z/, message: "7桁の数字で入力されていません" }
 end
