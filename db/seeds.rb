76550a6584234521111b4962a8b0c632c9b046fd
藤野貴明 <fujinotakaaki@fujinotakaakinoMacBook-Pro.local>
2020-03-04 20:27:53 +0900

    fix views. add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,23 +33,31 @@ Genre.create( name: '表示されないジャンル', is_displayed:  false )
 
 
 # Itemのサンプルデータ（全8件）
-Item.create( genre_id: 1, name:  %?ショートケーキ?, introduction: %?シンプルisベスト!当店人気No.1のケーキです!?,                listed_price: 500, image: File.open( './app/assets/images/cake1.png', ?r ) )
-Item.create( genre_id: 1, name: %?チョコレートケーキ?, introduction: %?バンホーテンのカカオ豆を使った濃厚チョコレートケーキ?, listed_price: 560, image: File.open( './app/assets/images/cake2.png', ?r ) )
-Item.create( genre_id: 1, name: %?チーズケーキ?, introduction: %?北海道産の最高級牛乳使用?,                                                   listed_price: 480, image: File.open( './app/assets/images/cake3.png', ?r ) )
-Item.create( genre_id: 1, name: %?ミルクレープ?, introduction: %?クレープ生地を15層に重ねたミルクレープです。?,               listed_price: 480, image: File.open( './app/assets/images/mille_crepe.png', ?r ) )
-Item.create( genre_id: 1, name: %?モンブラン?, introduction: %?秋季限定!ケーキの中にも栗がたっぷり入ったモンブラン!?,      listed_price: 580, image: File.open( './app/assets/images/montblanc.png', ?r ) )
-Item.create( genre_id: 1, name: %?アップルパイ?, introduction: %?サクッ！！あま〜い❤️シナモンとの相性抜群!長野県産のりんごを贅沢に使って作りました?, listed_price: 580, image: File.open( './app/assets/images/applepie.png', ?r ) )
-Item.create( genre_id: 2, name: %?スコーン?, introduction: %?スコーンとは、スコットランド料理の、バノックより重いパン。?, listed_price: 275, image: File.open( './app/assets/images/scone.png', ?r ) )
-Item.create( genre_id: 2, name: %?フルーツパウンドケーキ?, introduction: %?アプリコット、ラムレーズン、イチジク、オレンジを贅沢に使ったパウンドケーキ。?, listed_price: 1500, image: File.open( './app/assets/images/pound_cake.png', ?r ) )
+Item.create( genre_id: 8, name: %?Kejserens nye klæder?, introduction: %?「自分の地位にふさわしくない者」や「愚か者」には見えない極上の材料をふんだんに使用したケーキです。?, listed_price: 1_000_000, image: File.open( './app/assets/images/osara.png', ?r ) )
+Item.create( genre_id: 6, name: %?金太郎飴?, introduction: %?金太郎がデフォルメされた長さ30cmの金太郎飴です。（※切り売りは受け付けておりません）?, listed_price: 800, image: File.open( './app/assets/images/kintarou.png', ?r ) )
+Item.create( genre_id: 2, name: %?マカロン?, introduction: %?当店では新鮮なフルーツを使った３色のマカロンを用意しております。是非とも買ってください！！?, listed_price: 500, image: File.open( './app/assets/images/macaroon.png', ?r ) )
+Item.create( genre_id: 7, name: %?当店おすすめの、ケーキに合うスパークリングワインです。是非ともご一緒にお求めくださいませ。?, introduction: %??, listed_price: 5800, image: File.open( './app/assets/images/sparkling_wine1.png', ?r ) )
+Item.create( genre_id: 7, name: %?当店おすすめの、ケーキに合うスパークリングワインです。豪華な宴会にでもどうぞ。?, introduction: %??, listed_price: 30_000, image: File.open( './app/assets/images/sparkling_wine2.png', ?r ) )
+Item.create( genre_id: 1, name: %?イチゴのミルフィーユ?, introduction: %?北海道産牛乳を使ったカスタードクリームは絶品です。イチゴは「とちおとめ」をふんだんに使っております。?, listed_price: 880, image: File.open( './app/assets/images/mirufiyu_ichigo.png', ?r ) )
+Item.create( genre_id: 6, name: %?ペロペロキャンディー?, introduction: %?子供向のお菓子です。大人の方でも販売致しますので、ご安心を?, listed_price: 120, image: File.open( './app/assets/images/peropero_candy.png', ?r ) )
 Item.create( genre_id: 2, name: %?クッキー?, introduction: %?懐かしい味。素朴なクッキーの3枚セット?,                                   listed_price: 200, image: File.open( './app/assets/images/cookie.png', ?r ) )
 Item.create( genre_id: 3, name: %?チョコレート?, introduction: %?バンホーテンのカカオ豆を使ってますよ。?,                          listed_price: 100, image: File.open( './app/assets/images/chocolate.png', ?r ) )
 Item.create( genre_id: 4, name: %?カスタードプリン?, introduction: %?シンプルなプリンです。?,                                                listed_price: 120, image: File.open( './app/assets/images/purin.png', ?r ) )
 Item.create( genre_id: 4, name: %?ミルクプリン?, introduction: %?北海道産牛乳を使用したクリームが絶品。?,                          listed_price: 180, image: File.open( './app/assets/images/milk_purin.png', ?r ) )
-Item.create( genre_id: 5, name: %?羊羹?, introduction: %?北海道産小豆を使用してます。裏ごししてあるので舌触り滑らか。?, listed_price: 800, image: File.open( './app/assets/images/youkan.png', ?r ) )
-Item.create( genre_id: 6, name: %?アイスクリーム?, introduction: %?Häagen-Dazsのアイスなので間違いなく美味しいです。?, listed_price: 398, image: File.open( './app/assets/images/icecream.png', ?r ) )
-Item.create( genre_id: 8, name: %?Kejserens nye klæder?, introduction: %?「自分の地位にふさわしくない者」や「愚か者」には見えない極上の材料をふんだんに使用したケーキです。?, listed_price: 1_000_000, image: File.open( './app/assets/images/osara.png', ?r ) )
+Item.create( genre_id: 6, name: %?羊羹?, introduction: %?北海道産小豆を使用してます。裏ごししてあるので舌触り滑らか。?, listed_price: 800, image: File.open( './app/assets/images/youkan.png', ?r ) )
+Item.create( genre_id: 5, name: %?アイスクリーム?, introduction: %?Häagen-Dazsのアイスなので間違いなく美味しいです。?, listed_price: 398, image: File.open( './app/assets/images/icecream.png', ?r ) )
+Item.create( genre_id: 1, name: %?アップルパイ?, introduction: %?サクッ！！あま〜い❤️シナモンとの相性抜群!長野県産のりんごを贅沢に使って作りました?, listed_price: 580, image: File.open( './app/assets/images/applepie.png', ?r ) )
+Item.create( genre_id: 2, name: %?スコーン?, introduction: %?スコーンとは、スコットランド料理の、バノックより重いパン。?, listed_price: 275, image: File.open( './app/assets/images/scone.png', ?r ) )
+Item.create( genre_id: 2, name: %?フルーツパウンドケーキ?, introduction: %?アプリコット、ラムレーズン、イチジク、オレンジを贅沢に使ったパウンドケーキ。?, listed_price: 1500, image: File.open( './app/assets/images/pound_cake.png', ?r ) )
+Item.create( genre_id: 1, name: %?シフォンケーキ?, introduction: %?プレーンなシフォンケーキなので、どんなケーキにでも合います。クリームは自分で、どうぞ。?, listed_price: 1500, image: File.open( './app/assets/images/chiffoncake.png', ?r ) )
+Item.create( genre_id: 1, name:  %?ショートケーキ?, introduction: %?シンプルisベスト!当店人気No.1のケーキです!?,                listed_price: 500, image: File.open( './app/assets/images/cake1.png', ?r ) )
+Item.create( genre_id: 1, name: %?モンブラン?, introduction: %?秋季限定!ケーキの中にも栗がたっぷり入ったモンブラン!?,      listed_price: 580, image: File.open( './app/assets/images/montblanc.png', ?r ) )
+Item.create( genre_id: 1, name: %?ミルクレープ?, introduction: %?クレープ生地を15層に重ねたミルクレープです。?,               listed_price: 480, image: File.open( './app/assets/images/mille_crepe.png', ?r ) )
+Item.create( genre_id: 1, name: %?チーズケーキ?, introduction: %?北海道産の最高級牛乳使用?,                                                   listed_price: 480, image: File.open( './app/assets/images/cake3.png', ?r ) )
+Item.create( genre_id: 1, name: %?チョコレートケーキ?, introduction: %?バンホーテンのカカオ豆を使った濃厚チョコレートケーキ?, listed_price: 560, image: File.open( './app/assets/images/cake2.png', ?r ) )
+Item.create( genre_id: 8, name: %?サンプル画像?, introduction: %?#{'サンプル文書'*10}?, listed_price: 333, image: File.open( './app/assets/images/sample01.png', ?r ) )
 
-arr = (1..13).to_a.shuffle
+arr = (5..22).to_a.shuffle
 # arr = ( (1..13).to_a  - Item.find_by( genre_id: 8 )).shuffle
 # CartItemのサンプルデータ（全11件）
 CartItem.create( end_user_id: 1, item_id: arr.pop, amount: 2 )
@@ -65,26 +73,47 @@ CartItem.create( end_user_id: 3, item_id: arr.pop, amount: 31 )
 CartItem.create( end_user_id: 3, item_id: arr.pop, amount: 37 )
 CartItem.create( end_user_id: 3, item_id: arr.pop, amount: 3 )
 
-t = Time.zone.now - 1.day
-# Orderのサンプルデータ（全1件）
+t = Time.zone.now - 3.day
+# Orderのサンプルデータ
 Order.create( end_user_id: 1, postal_code: "1010041", ship_to: "東京都千代田区神田須田町１−２２", consignee: "株式会社ハル研究所", payment: "クレジットカード", postage: 800, total_price: 15585, order_status: "入金確認", created_at: t )
-Order.create( end_user_id: 1, postal_code: "1070052", ship_to: "東京都港区赤坂２丁目１２−２３ キャビンアリーナ赤坂 201", consignee: "株式会社京都アニメーション 東京オフィス", payment: "銀行振込", postage: 800, total_price: 40069, order_status: "入金確認" )
-Order.create( end_user_id: 2, postal_code: "1234567", ship_to: "国会議事堂", consignee: "安倍 晋三", payment: "銀行振込", postage: 800, total_price: 348862, order_status: "入金確認" )
-Order.create( end_user_id: 3, postal_code: "5830992", ship_to: "大阪府南河内郡太子町", consignee: "小野妹子", payment: "クレジットカード", postage: 800, total_price: 40356, order_status: "入金待ち" )
+OrderDetail.create( order_id: 1, item_id: 20, amount: 2, purchased_price: 1100, making_status: "製作完了", created_at: t )
+OrderDetail.create( order_id: 1, item_id: 12, amount: 3, purchased_price: 1650, making_status: "製作待ち", created_at: t )
+OrderDetail.create( order_id: 1, item_id: 13, amount: 5, purchased_price: 3080, making_status: "製作完了", created_at: t )
+OrderDetail.create( order_id: 1, item_id: 14, amount: 7, purchased_price: 3696, making_status: "着手不可", created_at: t )
+OrderDetail.create( order_id: 1, item_id: 15, amount: 11, purchased_price: 3322, making_status: "着手不可", created_at: t )
+OrderDetail.create( order_id: 1, item_id: 16, amount: 13, purchased_price: 1937, making_status: "製作待ち", created_at: t )
 
-# Orderdetailのサンプルデータ（全1件）
-OrderDetail.create( order_id: 1, item_id: 1, amount: 2, purchased_price: 1100, making_status: "製作完了", created_at: t )
-OrderDetail.create( order_id: 1, item_id: 2, amount: 3, purchased_price: 1650, making_status: "製作待ち", created_at: t )
-OrderDetail.create( order_id: 1, item_id: 3, amount: 5, purchased_price: 3080, making_status: "製作完了", created_at: t )
-OrderDetail.create( order_id: 1, item_id: 4, amount: 7, purchased_price: 3696, making_status: "着手不可", created_at: t )
-OrderDetail.create( order_id: 1, item_id: 5, amount: 11, purchased_price: 3322, making_status: "着手不可", created_at: t )
-OrderDetail.create( order_id: 1, item_id: 6, amount: 13, purchased_price: 1937, making_status: "製作待ち", created_at: t )
-OrderDetail.create( order_id: 2, item_id: 14, amount: 13, purchased_price: 5681, making_status: "着手不可" )
-OrderDetail.create( order_id: 2, item_id: 11, amount: 19, purchased_price: 2508, making_status: "製作中" )
-OrderDetail.create( order_id: 2, item_id: 8, amount: 120, purchased_price: 31080, making_status: "製作中" )
-OrderDetail.create( order_id: 3, item_id: 1, amount: 177, purchased_price: 97350, making_status: "製作中" )
-OrderDetail.create( order_id: 3, item_id: 2, amount: 196, purchased_price: 107800, making_status: "製作中" )
-OrderDetail.create( order_id: 3, item_id: 3, amount: 232, purchased_price: 142912, making_status: "製作中" )
-OrderDetail.create( order_id: 4, item_id: 3, amount: 31, purchased_price: 19096, making_status: "着手不可" )
-OrderDetail.create( order_id: 4, item_id: 2, amount: 37, purchased_price: 20350, making_status: "着手不可" )
+
+t = Time.zone.now - 2.day
+Order.create( end_user_id: 1, postal_code: "1070052", ship_to: "東京都港区赤坂２丁目１２−２３ キャビンアリーナ赤坂 201", consignee: "株式会社京都アニメーション 東京オフィス", payment: "銀行振込", postage: 800, total_price: 40069, order_status: "入金確認", created_at: t  )
+OrderDetail.create( order_id: 2, item_id: 14, amount: 13, purchased_price: 5681, making_status: "着手不可", created_at: t  )
+OrderDetail.create( order_id: 2, item_id: 11, amount: 19, purchased_price: 2508, making_status: "製作中", created_at: t  )
+OrderDetail.create( order_id: 2, item_id: 18, amount: 120, purchased_price: 31080, making_status: "製作中", created_at: t  )
+
+
+t = Time.zone.now - 1.day
+Order.create( end_user_id: 2, postal_code: "1234567", ship_to: "国会議事堂", consignee: "安倍 晋三", payment: "銀行振込", postage: 800, total_price: 348862, order_status: "入金確認", created_at: t  )
+OrderDetail.create( order_id: 3, item_id: 20, amount: 177, purchased_price: 97350, making_status: "製作中", created_at: t  )
+OrderDetail.create( order_id: 3, item_id: 12, amount: 196, purchased_price: 107800, making_status: "製作中", created_at: t  )
+OrderDetail.create( order_id: 3, item_id: 13, amount: 232, purchased_price: 142912, making_status: "製作中", created_at: t  )
+
+
+Order.create( end_user_id: 3, postal_code: "5830992", ship_to: "大阪府南河内郡太子町", consignee: "小野妹子", payment: "クレジットカード", postage: 800, total_price: 40356, order_status: "入金待ち" )
+OrderDetail.create( order_id: 4, item_id: 13, amount: 31, purchased_price: 19096, making_status: "着手不可" )
+OrderDetail.create( order_id: 4, item_id: 12, amount: 37, purchased_price: 20350, making_status: "着手不可" )
 OrderDetail.create( order_id: 4, item_id: 10, amount: 1, purchased_price: 110, making_status: "着手不可" )
+
+
+Order.create(end_user_id: 1, postal_code: "1234567", ship_to: "東京都", consignee: "藤野 貴明", payment: "クレジットカード", postage: 800, total_price: 245836, order_status: "入金待ち" )
+OrderDetail.create( order_id: 5, item_id: 14, amount: 2, purchased_price: 1276, making_status: "製作完了" )
+OrderDetail.create( order_id: 5, item_id: 7, amount: 3, purchased_price: 396, making_status: "製作完了" )
+OrderDetail.create( order_id: 5, item_id: 6, amount: 5, purchased_price: 4840, making_status: "製作完了" )
+OrderDetail.create( order_id: 5, item_id: 5, amount: 7, purchased_price: 231000, making_status: "製作待ち" )
+OrderDetail.create( order_id: 5, item_id: 20, amount: 11, purchased_price: 5808, making_status: "製作待ち" )
+OrderDetail.create( order_id: 5, item_id: 10, amount: 13, purchased_price: 1716, making_status: "製作中" )
+
+
+Order.create(end_user_id: 1, postal_code: "5557777", ship_to: "toukyo", consignee: "test", payment: "クレジットカード", postage: 800, total_price: 10106, order_status: "入金待ち" )
+OrderDetail.create( order_id: 6, item_id: 65, amount: 13, purchased_price: 6864, making_status: "製作待ち" )
+OrderDetail.create( order_id: 6, item_id: 60, amount: 1, purchased_price: 1650, making_status: "製作中" )
+OrderDetail.create( order_id: 6, item_id: 55, amount: 4, purchased_price: 792, making_status: "着手不可" )
